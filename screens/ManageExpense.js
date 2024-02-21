b178bc67c560695dd3936fa7cf58c5d0e2e70c78
DuBing <bdu.wbrq@hongshan.com>
2024-02-21 17:47:09 +0800

    customize button

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -2,6 +2,7 @@ import React, { useLayoutEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../constants/styles";
 import IconButton from "../components/UI/IconButton";
+import Button from "../components/UI/Button";
 
 const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -17,8 +18,24 @@ const ManageExpense = ({ route, navigation }) => {
     console.log("deleting");
   };
 
+  const cancelHandler = () => {
+    console.log("canceling");
+  };
+
+  const confirmHandler = () => {
+    console.log("confirm");
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -40,6 +57,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
