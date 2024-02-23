a726cf837f66656219f2fab16f826d5f8c99ed5a
DuBing <bdu.wbrq@hongshan.com>
2024-02-23 16:26:42 +0800

    Implement Section 11: User Authentication starting code

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
+import IconButton from "./components/UI/IconButton";
+import { Colors, GlobalStyles } from "./constants/styles";
 import AllExpenses from "./screens/AllExpenses";
+import LoginScreen from "./screens/LoginScreen";
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { GlobalStyles } from "./constants/styles";
-import IconButton from "./components/UI/IconButton";
+import SignupScreen from "./screens/SignupScreen";
 import ExpensesContextProvider from "./store/expense-context";
 
 const Stack = createNativeStackNavigator();
@@ -62,13 +64,29 @@ const ExpensesOverview = () => {
     </BottomTabs.Navigator>
   );
 };
+
+function AuthStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.primary100 },
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
       <ExpensesContextProvider>
         <NavigationContainer>
-          <Stack.Navigator
+          {/* <Stack.Navigator
             screenOptions={{
               headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
               headerTintColor: "white",
@@ -84,7 +102,8 @@ export default function App() {
               component={ManageExpense}
               options={{ presentation: "modal" }}
             />
-          </Stack.Navigator>
+          </Stack.Navigator> */}
+          <AuthStack />
         </NavigationContainer>
       </ExpensesContextProvider>
     </>
--- constants/styles.js
+++ constants/styles.js
@@ -14,3 +14,11 @@ export const GlobalStyles = {
     gray700: "#221c30",
   },
 };
+
+export const Colors = {
+  primary100: "#f9beda",
+  primary500: "#c30b64",
+  primary800: "#610440",
+  error100: "#fcdcbf",
+  error500: "#f37c13",
+};
